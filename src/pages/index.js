e580d2196a84bfa0c00a50eea3ade948a2fd46d6
Jorge Luis Beroisa <jberoisa@gmail.com>
2018-05-25 16:31:43 -0300

    blog2

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,6 @@ import Link from 'gatsby-link'
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
-
     return (
       <section className="section">
         <div className="container">
